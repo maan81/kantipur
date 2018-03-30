d186d76253e83f0949c0fe3a3b22cb89adb07b7b
Pranij Man Shrestha <pranijman@gmail.com>
2018-03-30 11:33:49 +0545

    adding style & js

--- index.php
+++ index.php
@@ -3,6 +3,8 @@
 <head>
     <meta charset="utf-8"/>
     <title>Kantipur TV</title>
+    <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
+    <link rel="stylesheet" type="text/css" href="style.css">
 </head>
 <body>
 
@@ -10,7 +12,9 @@
 
     <div id="player"></div>
 
-    <div id="playlist"></div>
+<script src="//code.jquery.com/jquery-3.2.1.slim.min.js" ></script>
+<script src="//cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"></script>
+<script src="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
 
 </body>
 </html>
