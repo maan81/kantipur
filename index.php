20ef3090c5a7ee30084926cd634ba2dcde2f7c34
Pranij Man Shrestha <pranijman@gmail.com>
2018-03-30 11:34:03 +0545

    new desigm

--- index.php
+++ index.php
@@ -8,9 +8,102 @@
 </head>
 <body>
 
+<div class="container">
+
     <h2>What the flop ?</h2>
 
-    <div id="player"></div>
+    <div class="row">
+
+        <div id="video_wrapper" class="col-lg-6">
+            <div id="player"></div>
+            <div id="player_wrapper">
+                <div id="player_wrapper" class="col-md-12">
+                    <h5>Playlist</h5>
+                    <div id="playlist" class="row">
+                        <div class="link col-md-4">
+                            <a href="#">
+                                <img src="https://i.ytimg.com/vi/IMlQFgRacQU/hqdefault.jpg?sqp=-oaymwEWCKgBEF5IWvKriqkDCQgBFQAAiEIYAQ==&rs=AOn4CLC8_YBDy_UsqhRIzDQ5FsoSE8QByw" alt="Video Thumbs">
+                            </a>
+                        </div>
+                        <div class="link col-md-4">
+                            <a href="#">
+                                <img src="https://i.ytimg.com/vi/IMlQFgRacQU/hqdefault.jpg?sqp=-oaymwEWCKgBEF5IWvKriqkDCQgBFQAAiEIYAQ==&rs=AOn4CLC8_YBDy_UsqhRIzDQ5FsoSE8QByw" alt="Video Thumbs">
+                            </a>
+                        </div>
+                        <div class="link col-md-4">
+                            <a href="#">
+                                <img src="https://i.ytimg.com/vi/IMlQFgRacQU/hqdefault.jpg?sqp=-oaymwEWCKgBEF5IWvKriqkDCQgBFQAAiEIYAQ==&rs=AOn4CLC8_YBDy_UsqhRIzDQ5FsoSE8QByw" alt="Video Thumbs">
+                            </a>
+                        </div>
+                        <div class="link col-md-4">
+                            <a href="#">
+                                <img src="https://i.ytimg.com/vi/IMlQFgRacQU/hqdefault.jpg?sqp=-oaymwEWCKgBEF5IWvKriqkDCQgBFQAAiEIYAQ==&rs=AOn4CLC8_YBDy_UsqhRIzDQ5FsoSE8QByw" alt="Video Thumbs">
+                            </a>
+                        </div>
+                        <div class="link col-md-4">
+                            <a href="#">
+                                <img src="https://i.ytimg.com/vi/IMlQFgRacQU/hqdefault.jpg?sqp=-oaymwEWCKgBEF5IWvKriqkDCQgBFQAAiEIYAQ==&rs=AOn4CLC8_YBDy_UsqhRIzDQ5FsoSE8QByw" alt="Video Thumbs">
+                            </a>
+                        </div>
+                        <div class="link col-md-4">
+                            <a href="#">
+                                <img src="https://i.ytimg.com/vi/IMlQFgRacQU/hqdefault.jpg?sqp=-oaymwEWCKgBEF5IWvKriqkDCQgBFQAAiEIYAQ==&rs=AOn4CLC8_YBDy_UsqhRIzDQ5FsoSE8QByw" alt="Video Thumbs">
+                            </a>
+                        </div>
+                        <div class="link col-md-4">
+                            <a href="#">
+                                <img src="https://i.ytimg.com/vi/IMlQFgRacQU/hqdefault.jpg?sqp=-oaymwEWCKgBEF5IWvKriqkDCQgBFQAAiEIYAQ==&rs=AOn4CLC8_YBDy_UsqhRIzDQ5FsoSE8QByw" alt="Video Thumbs">
+                            </a>
+                        </div>
+                        <div class="link col-md-4">
+                            <a href="#">
+                                <img src="https://i.ytimg.com/vi/IMlQFgRacQU/hqdefault.jpg?sqp=-oaymwEWCKgBEF5IWvKriqkDCQgBFQAAiEIYAQ==&rs=AOn4CLC8_YBDy_UsqhRIzDQ5FsoSE8QByw" alt="Video Thumbs">
+                            </a>
+                        </div>
+                        <div class="link col-md-4">
+                            <a href="#">
+                                <img src="https://i.ytimg.com/vi/IMlQFgRacQU/hqdefault.jpg?sqp=-oaymwEWCKgBEF5IWvKriqkDCQgBFQAAiEIYAQ==&rs=AOn4CLC8_YBDy_UsqhRIzDQ5FsoSE8QByw" alt="Video Thumbs">
+                            </a>
+                        </div>
+                    </div>
+                    <!-- /playlist -->
+                </div>
+                <!-- /playlist -->
+            </div>
+            <!-- /player_wrapper -->
+        </div>
+
+        <div id="comment_wrapper" class="col-lg-6">
+            <div class="comment_submit">
+                <textarea class="col-lg-12" placeholder="Your Comments"></textarea>
+                <button class="btn btn-default btn-outline-primary float-right">Post</button>
+            </div>
+            <div class="clearfix"></div>
+            <div class="comment_existing">
+                <div class="comment">
+                    <small class="text-secondary">Someone Wrote :</small>
+                    <p>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla id dolor efficitur, tincidunt purus vitae, sagittis massa. Ut ullamcorper lectus ex, sit amet mollis eros consectetur ut. In hac habitasse platea dictumst. Fusce magna enim, fringilla nec mattis nec, efficitur non ligula. Proin posuere non quam sit amet elementum. Praesent eget lorem et metus varius aliquam. Donec urna est, sagittis at dolor sit amet, bibendum aliquam ex.
+                    </p>
+                </div>
+                <div class="comment">
+                    <small class="text-secondary">Someone Wrote :</small>
+                    <p>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla id dolor efficitur, tincidunt purus vitae, sagittis massa. Ut ullamcorper lectus ex, sit amet mollis eros consectetur ut. In hac habitasse platea dictumst. Fusce magna enim, fringilla nec mattis nec, efficitur non ligula. Proin posuere non quam sit amet elementum. Praesent eget lorem et metus varius aliquam. Donec urna est, sagittis at dolor sit amet, bibendum aliquam ex.
+                    </p>
+                </div>
+                <div class="comment">
+                    <small class="text-secondary">Someone Wrote :</small>
+                    <p>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla id dolor efficitur, tincidunt purus vitae, sagittis massa. Ut ullamcorper lectus ex, sit amet mollis eros consectetur ut. In hac habitasse platea dictumst. Fusce magna enim, fringilla nec mattis nec, efficitur non ligula. Proin posuere non quam sit amet elementum. Praesent eget lorem et metus varius aliquam. Donec urna est, sagittis at dolor sit amet, bibendum aliquam ex.
+                    </p>
+                </div>
+            </div>
+        </div>
+
+    </div>
+</div>
+<!-- /container -->
 
 <script src="//code.jquery.com/jquery-3.2.1.slim.min.js" ></script>
 <script src="//cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"></script>
