bad26394e79a65fee5ee79ea1db1e57b1c3f47ac
Pranij Man Shrestha <pranijman@gmail.com>
2018-03-31 22:30:31 +0545

    configuring video width

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 
     var tag = document.createElement('script');
 
-    tag.src = "https://www.youtube.com/iframe_api";
+    // tag.src = "https://www.youtube.com/iframe_api";
+    tag.src = "https://youtube.com/iframe_api";
     var firstScriptTag = document.getElementsByTagName('script')[0];
     firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
 
